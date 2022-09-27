2b3506cfbc311a0284748c812827086bc41c50a2
Marc Walter <m.walter-dev@gmx.de>
2022-09-27 16:47:03 +0200

    add js nextQuestion()

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let score = 15;
+
 function init() {
     const content = document.getElementById('content');
     content.innerHTML = returnHtmlCard();
-}
+    }
 
 function answer(answerNumber) {
     let rightAnswerNumber = questions[currentQuestion]["right_answer"];
@@ -10,11 +12,32 @@ function answer(answerNumber) {
     if (answerNumber == rightAnswerNumber) {
         console.log('right');
         document.getElementById(idRightAnswer).classList.add('bg-success');
+        document.getElementById('next-btn').disabled = false;
+        disableAllAnswers ();
     } else {
         console.log('false');
-        document.getElementById(idRightAnswer).classList.add('bg-success');
-        document.getElementById(idAnswer).classList.add('bg-danger');
+        document.getElementById(idAnswer).classList.add('bg-secondary');
+        score -= 1
+        console.log(score);
+        document.getElementById(idAnswer).disabled = true;
+        document.getElementById(idAnswer).classList.remove('pointer');
     }
+}
+
+function disableAllAnswers () {
+        document.getElementById('answer_1').disabled = true;
+        document.getElementById('answer_2').disabled = true;
+        document.getElementById('answer_3').disabled = true;
+        document.getElementById('answer_4').disabled = true;
+        document.getElementById('answer_1').classList.remove('pointer', 'answer');
+        document.getElementById('answer_2').classList.remove('pointer', 'answer');
+        document.getElementById('answer_3').classList.remove('pointer', 'answer');
+        document.getElementById('answer_4').classList.remove('pointer', 'answer');
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    init();
 }
 
 //----------------------HTML code-------------------------------------------
@@ -25,40 +48,43 @@ function returnHtmlCard() {
     let answer_2 = questions[currentQuestion]["answer_2"];
     let answer_3 = questions[currentQuestion]["answer_3"];
     let answer_4 = questions[currentQuestion]["answer_4"];
+    let questionNumber = currentQuestion + 1;
         return `
     <div class="card" style="width: 18rem;">
             <img src="img/questions_img/${img}" class="card-img-top card-img-small" alt="...">
             <div class="card-middle flex">
 
                 <div class="card-body">
-                    <h5 class="card-title mb-3">${question}</h5>
+                    <h5 class="card-title mb-3">
+                        ${question}
+                    </h5>
 
-                    <div id="answer_1" class="card mt-2 pointer answer" onclick="answer(1)">
+                    <button id="answer_1" class="card mt-2 pointer answer button-answer" onclick="answer(1)">
                         <div class="card-body ">
                             ${answer_1}
                         </div>
-                    </div>
-                    <div id="answer_2" class="card mt-2 pointer answer" onclick="answer(2)">
+                    </button>
+                    <button id="answer_2" class="card mt-2 pointer answer button-answer" onclick="answer(2)">
                         <div class="card-body">
-                        ${answer_2}
+                            ${answer_2}
                         </div>
-                    </div>
-                    <div id="answer_3" class="card mt-2 pointer answer" onclick="answer(3)">
+                    </button>
+                    <button id="answer_3" class="card mt-2 pointer answer button-answer" onclick="answer(3)">
                         <div class="card-body">
-                        ${answer_3}
+                            ${answer_3}
                         </div>
-                    </div>
-                    <div id="answer_4" class="card mt-2 pointer answer" onclick="answer(4)">
+                    </button>
+                    <button id="answer_4" class="card mt-2 pointer answer button-answer" onclick="answer(4)">
                         <div class="card-body">
-                        ${answer_4}
+                            ${answer_4}
                         </div>
-                    </div>
+                    </button>
                 </div>
 
             </div>
             <div class="card-footer">
-                <span><b>1</b> von <b>${questions.length}</b></span>
-                <a href="#" class="btn btn-primary pointer">Nächste Frage</a>
+                <span><b>${questionNumber}</b> von <b>${questions.length}</b></span>
+                <button disabled id="next-btn" href="#" class="btn btn-primary pointer" onclick="nextQuestion()">Nächste Frage</button>
             </div>
         </div>
     `;
