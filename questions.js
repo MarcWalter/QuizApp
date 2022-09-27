781f505ef537a18bef3b4a1d5e0a060f464094e5
Marc Walter <m.walter-dev@gmx.de>
2022-09-27 11:21:59 +0200

    add questions

--- questions.js
+++ questions.js
@@ -1,128 +1,128 @@
-let questions_1 = [
+let questions_planets = [
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Wann geht die Sonne auf?",
+        "answer_1": "Morgens",
+        "answer_2": "Mittags",
+        "answer_3": "Abends",
+        "answer_4": "Nachts",
+        "right_answer": 1
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Wann geht die Sonne auf dem Mond auf?",
+        "answer_1": "Morgens",
+        "answer_2": "Mittags",
+        "answer_3": "Abends",
+        "answer_4": "Nachts",
+        "right_answer": 1
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Welcher Planet ist der größte unseres Sonnensystems?",
+        "answer_1": "Neptun",
+        "answer_2": "Venus",
+        "answer_3": "Jupiter",
+        "answer_4": "Saturn",
+        "right_answer": 3
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Wieviel mal weiter ist der Mond von der Erde entfernt, als die ISS von der Erde?",
+        "answer_1": "ca. 2 Mal weiter",
+        "answer_2": "ca. 10 Mal weiter",
+        "answer_3": "ca. 100 Mal weiter",
+        "answer_4": "ca. 1.000 Mal weiter",
+        "right_answer": 4
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Wieviel mal weiter ist der Mars von der Erde entfernt, als der Mond von der Erde?",
+        "answer_1": "ca. 10 Mal weiter",
+        "answer_2": "ca. 100 Mal weiter",
+        "answer_3": "ca. 1.000 Mal weiter",
+        "answer_4": "ca. 10.000 Mal weiter",
+        "right_answer": 3
     }
 ];
 
-let questions_2 = [
+let questions_animals = [
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Wie groß sind Känguru-Babys, wenn sie auf die Welt kommen?",
+        "answer_1": "2 mm",
+        "answer_2": "2 cm",
+        "answer_3": "20 cm",
+        "answer_4": "Ist bis jetzt nicht bekannt",
+        "right_answer": 2
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Welche Funktion erfüllt der Kamelhöcker?",
+        "answer_1": "Fettspeicher",
+        "answer_2": "Wasserspeicher",
+        "answer_3": "Wiederkäuermagen",
+        "answer_4": "'Sitz des Orientierungssinnes",
+        "right_answer": 1
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Was können Erdferkel besonders gut?",
+        "answer_1": "Als Spührschweine arbeiten",
+        "answer_2": "Trüffel finden",
+        "answer_3": "Erdhöhlen bauen",
+        "answer_4": "Hitze aushalten",
+        "right_answer": 3
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Wie alt können Schildkröten werden",
+        "answer_1": "100 Jahre",
+        "answer_2": "150 Jahre",
+        "answer_3": "200 Jahre",
+        "answer_4": "250 Jahre",
+        "right_answer": 4
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
+        "question": "Wie lange können Seehunde die Luft anhalten?",
+        "answer_1": "30 min",
+        "answer_2": "40 min",
+        "answer_3": "50 min",
+        "answer_4": "60 min",
+        "right_answer": 1
     }
 ];
 
-let questions_3 = [
+let questions_jesus = [
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
+        "question": "Welche Aussage stammt nicht von Jesus Christus gemäß der Bibel?",
+        "answer_1": "Wer zu seinem Bruder 'Idiot' sagt gehört ins Feuer der Hölle.",
+        "answer_2": "Euer Ja sei ein Ja und euer Nein ein Nein!",
+        "answer_3": "Zerstört diesen Tempel und ich werde ihn in drei Tagen wieder aufbauen.",
+        "answer_4": "Wenn ihr schon redet, dann auch die Wahrheit.",
+        "right_answer": 4
+    },
+    {
+        "question": "Welche Aussage stammt nicht von Jesus Christus gemäß der Bibel?",
+        "answer_1": "Die Qualen meiner Seele bringen mich fast um.",
+        "answer_2": "Es ist besser mit leeren Händen zum Tempel zu kommen als gar nicht.",
+        "answer_3": "Mir ist alle Macht im Himmel und auf Erden gegeben.",
+        "answer_4": "Mein Gott, mein Gott, warum hast du mich verlassen?",
         "right_answer": 
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
+        "question": "Welche Aussage stammt nicht von Jesus Christus gemäß der Bibel?",
+        "answer_1": "Steck dein Schwert weg!",
+        "answer_2": "Ich gehe noch ein Stück weiter um zu beten.",
+        "answer_3": "Meine Worte vergehen nie.",
+        "answer_4": "Was fragst du mich das? Geh zu den Priestern.",
         "right_answer": 
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
+        "question": "Welche Aussage stammt nicht von Jesus Christus gemäß der Bibel?",
+        "answer_1": "Komm, folge mir nach.",
+        "answer_2": "Nur Narren verachten Reichtum.",
+        "answer_3": "Wer an den Sohn glaubt, hat ewiges Leben.",
+        "answer_4": "Meine Nahrung ist, dass ich den Willen Gottes tue.",
         "right_answer": 
     },
     {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
-        "right_answer": 
-    },
-    {
-        "question": "",
-        "answer_1": "",
-        "answer_2": "",
-        "answer_3": "",
-        "answer_4": "",
+        "question": "Welche Aussage stammt nicht von Jesus Christus gemäß der Bibel?",
+        "answer_1": "Gott hat die Wege der Menschem im Blick.",
+        "answer_2": "Gott hat seinen Sohn nicht in die Welt geschickt, um sie zu verurteilen, sondern um sie durch ihn zu retten.",
+        "answer_3": "Ihr sucht mich nur, weil ihr satt geworden seid.",
+        "answer_4": "An jenem letzten Tag werde ich ihn von den Toten auferwecken.",
         "right_answer": 
     }
 ];
